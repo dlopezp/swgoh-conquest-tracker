ccd4741ec5744841a4b0adea2ca73f52ba7f6fb2
Daniel López Pérez <dlopezp.dev@gmail.com>
2022-02-06 19:12:06 +0100

    fix: gh-pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: process.env.ROUTER_BASE || '/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'swgoh-conquest-tracker',
