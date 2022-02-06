bd2ec4ec9e90aaaabf0504264edd0531e7d3156b
Daniel López Pérez <dlopezp.dev@gmail.com>
2022-02-06 19:44:28 +0100

    fix: rollbar

--- plugins/rollbar.js
+++ plugins/rollbar.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Rollbar from 'rollbar'
 
-Vue.prototype.$rollbar = new Rollbar({
+const rollbar = new Rollbar({
   accessToken: '93890c05fb024441b401331d24e79183',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
+
+export default (ctx, inject) => {
+  ctx.$rollbar = rollbar
+  inject('rollbar', rollbar)
+  Vue.rollbar = rollbar
+}
 
 Vue.config.errorHandler = (err, vm, info) => {
   vm.$rollbar.error(err);
