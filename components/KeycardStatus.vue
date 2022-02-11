223a35aef60b02d77ca1bda7b8310d1ca1d87067
Daniel López Pérez <dlopezp.dev@gmail.com>
2022-02-11 16:35:46 +0100

    fix: KeycardsStatus from store

--- components/KeycardStatus.vue
+++ components/KeycardStatus.vue
@@ -24,15 +24,13 @@
 
 <script>
 import common from '~/mixins/common'
-import { keycardsByConquest } from '~/src/sector'
 
 export default {
   mixins: [common],
   computed: {
     keycardsMax () {
-      if (!this.tracker.mode) { return 0 }
-      const conquest = this.conquest[this.tracker.mode]
-      return keycardsByConquest(conquest)
+      if (!this.tracker) { return 0 }
+      return this.$store.getters.totalKeycardsInMode(this.tracker.mode)
     },
     keycardsNeeded () {
       if (!this.tracker) { return 0 }
@@ -46,70 +44,12 @@ export default {
     keycardsStarred () {
       const mode = this.tracker.mode
       if (!mode) { return 0 }
-      const conquest = this.conquest[mode]
-      return this.starredKeycardsInConquest(conquest)
+      return this.$store.getters.starredKeycardsInMode(mode)
     },
     keycardsDone () {
       const mode = this.tracker.mode
       if (!mode) { return 0 }
-      const conquest = this.conquest[mode]
-      return this.doneKeycardsInConquest(conquest)
-    },
-    starredIds () {
-      return this.tracker.starred || []
-    },
-    doneIds () {
-      return this.tracker.done || []
-    }
-  },
-  methods: {
-    starredKeycardsInConquest (conquest) {
-      return this.starredKeycardsInSector(conquest.sector1) +
-        this.starredKeycardsInSector(conquest.sector2) +
-        this.starredKeycardsInSector(conquest.sector3) +
-        this.starredKeycardsInSector(conquest.sector4) +
-        this.starredKeycardsInSector(conquest.sector5) +
-        this.starredKeycardsInZone(conquest.global.feats)
-    },
-    starredKeycardsInSector (sector) {
-      return this.starredKeycardsInSectorNodes(sector) +
-        this.starredKeycardsInZone(sector.nodesFeats) +
-        this.starredKeycardsInZone(sector.minibossFeats) +
-        this.starredKeycardsInZone(sector.bossFeats)
-    },
-    starredKeycardsInSectorNodes (sector) {
-      return this.tracker[sector.id]?.nodesKeycardsStarred || 0
-    },
-    starredKeycardsInZone (zone) {
-      return zone.reduce(
-        (carry, feat) => {
-          return carry + (this.starredIds.includes(feat.id) ? feat.keycards : 0)
-        },
-        0
-      )
-    },
-    doneKeycardsInConquest (conquest) {
-      return this.doneKeycardsInSector(conquest.sector1) +
-        this.doneKeycardsInSector(conquest.sector2) +
-        this.doneKeycardsInSector(conquest.sector3) +
-        this.doneKeycardsInSector(conquest.sector4) +
-        this.doneKeycardsInSector(conquest.sector5) +
-        this.doneKeycardsInZone(conquest.global.feats)
-    },
-    doneKeycardsInSector (sector) {
-      return this.doneKeycardsInSectorNodes(sector) +
-        this.doneKeycardsInZone(sector.nodesFeats) +
-        this.doneKeycardsInZone(sector.minibossFeats) +
-        this.doneKeycardsInZone(sector.bossFeats)
-    },
-    doneKeycardsInSectorNodes (sector) {
-      return this.tracker[sector.id]?.nodesKeycardsDone || 0
-    },
-    doneKeycardsInZone (zone) {
-      return zone.reduce(
-        (carry, feat) => carry + this.doneIds.includes(feat.id) ? feat.keycards : 0,
-        0
-      )
+      return this.$store.getters.doneKeycardsInMode(mode)
     }
   }
 }
