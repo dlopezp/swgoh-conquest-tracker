fdb034583f5bdaa0e2453d2c08bfd35b98a29db1
Daniel López Pérez <dlopezp.dev@gmail.com>
2022-02-11 16:31:24 +0100

    fix: load Sentry only in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,77 @@
 const isDev = process.env.NODE_ENV === 'development'
+const isProduction = process.env.NODE_ENV === 'production'
+
+const modules = [
+  // https://go.nuxtjs.dev/axios
+  '@nuxtjs/axios',
+  // https://go.nuxtjs.dev/pwa
+  '@nuxtjs/pwa',
+  // https://go.nuxtjs.dev/content
+  '@nuxt/content',
+  [
+    '@nuxtjs/i18n',
+    {
+      locales: [
+        { code: 'en', iso: 'en-us', file: 'en.json', name: 'english' },
+        { code: 'es', iso: 'es-es', file: 'es.json', name: 'castellano' }
+      ],
+      detectBrowserLanguage: {
+        useCookie: true,
+        cookieKey: 'i18n_redirected',
+        redirectOn: 'root'
+      },
+      defaultLocale: 'en',
+      vueI18nLoader: true,
+      langDir: '~/locales/',
+      vueI18n: {
+        fallbackLocale: 'en'
+      },
+      strategy: 'no_prefix'
+    }
+  ],
+  [
+    '@nuxtjs/firebase',
+    {
+      config: {
+        apiKey: 'AIzaSyAfzvdCwDd7cNc2HgOEo0gns-JoCVTj6us',
+        authDomain: 'swgoh-conquest-tracker.firebaseapp.com',
+        projectId: 'swgoh-conquest-tracker',
+        storageBucket: 'swgoh-conquest-tracker.appspot.com',
+        messagingSenderId: '377589165565',
+        appId: '1:377589165565:web:194eaa7fb891a2ca25fd94',
+        measurementId: 'G-DGCW7XNSBC'
+      },
+      services: {
+        firestore: {
+          memoryOnly: false, // default
+          chunkName: process.env.NODE_ENV !== 'production' ? 'firebase-auth' : '[id]', // default
+          enablePersistence: true,
+          emulatorPort: isDev ? 8080 : undefined,
+          emulatorHost: 'localhost',
+          settings: {
+            // Firestore Settings - currently only works in SPA mode
+          }
+        },
+        auth: {
+          persistence: 'local',
+          initialize: {
+            onAuthStateChangedMutation: 'ON_AUTH_STATE_CHANGED_MUTATION',
+            onAuthStateChangedAction: 'onAuthStateChangedAction',
+            subscribeManually: false
+          },
+          ssr: false,
+          emulatorPort: isDev ? 9099 : undefined,
+          emulatorHost: 'http://localhost'
+        },
+        analytics: true
+      }
+    }
+  ]
+]
+
+if (isProduction) {
+  modules.push('@nuxtjs/sentry')
+}
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
@@ -57,74 +130,7 @@ export default {
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-    '@nuxtjs/sentry',
-    [
-      '@nuxtjs/i18n',
-      {
-        locales: [
-          { code: 'en', iso: 'en-us', file: 'en.json', name: 'english' },
-          { code: 'es', iso: 'es-es', file: 'es.json', name: 'castellano' }
-        ],
-        detectBrowserLanguage: {
-          useCookie: true,
-          cookieKey: 'i18n_redirected',
-          redirectOn: 'root'
-        },
-        defaultLocale: 'en',
-        vueI18nLoader: true,
-        langDir: '~/locales/',
-        vueI18n: {
-          fallbackLocale: 'en'
-        },
-        strategy: 'no_prefix'
-      }
-    ],
-    [
-      '@nuxtjs/firebase',
-      {
-        config: {
-          apiKey: 'AIzaSyAfzvdCwDd7cNc2HgOEo0gns-JoCVTj6us',
-          authDomain: 'swgoh-conquest-tracker.firebaseapp.com',
-          projectId: 'swgoh-conquest-tracker',
-          storageBucket: 'swgoh-conquest-tracker.appspot.com',
-          messagingSenderId: '377589165565',
-          appId: '1:377589165565:web:194eaa7fb891a2ca25fd94',
-          measurementId: 'G-DGCW7XNSBC'
-        },
-        services: {
-          firestore: {
-            memoryOnly: false, // default
-            chunkName: process.env.NODE_ENV !== 'production' ? 'firebase-auth' : '[id]', // default
-            enablePersistence: true,
-            emulatorPort: isDev ? 8080 : undefined,
-            emulatorHost: 'localhost',
-            settings: {
-              // Firestore Settings - currently only works in SPA mode
-            }
-          },
-          auth: {
-            persistence: 'local',
-            initialize: {
-              onAuthStateChangedMutation: 'ON_AUTH_STATE_CHANGED_MUTATION',
-              onAuthStateChangedAction: 'onAuthStateChangedAction',
-              subscribeManually: false
-            },
-            ssr: false,
-            emulatorPort: isDev ? 9099 : undefined,
-            emulatorHost: 'http://localhost'
-          },
-          analytics: true
-        }
-      }
-    ]
-  ],
+  modules,
 
   sentry: {
     dsn: 'https://a6e03ba5693c49c38e2cc92335a49acc@o1137009.ingest.sentry.io/6189136',
